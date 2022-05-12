5d5320af1f1d46e86cd8723b3d1e583b518d4359
richr <baldwinrich11510@gmail.com>
2022-05-12 00:43:42 -0400

    updated flask

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
-from flask import Flask,render_template,request,url_for
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
-#add comment
-
-def generate_html(message):
-    version_number = '007'
-    html = """
-        <html>
-        <body>
-            <div style='text-align:center;font-size:80px;'>
-                <br> {0}
-                <p>Version Number: {1}</p>
-                <br>
-                <image height="800" width="500" src="https://raw.githubusercontent.com/richardrengel/complaint_triage/main/images/IMG-8086.jpg">
-                
-            </div>
-        </body>
-        </html>""".format(message,version_number)
-    return html
-
-def greet():
-    greeting = 'Hi, from Mushroom'
-    return greeting
 
 
+def predict_label(img_path):
+	i = image.load_img(img_path, target_size=(100,100))
+	i = image.img_to_array(i)/255.0
+	i = i.reshape(1, 100,100,3)
+	p = model.predict_classes(i)
+	return dic[p[0]]
 
 
-# @app.route("/")
-# def hello_world():
-#     html = generate_html(greet())
-#     return html
-@app.route("/")
-def index():
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
 	return render_template("index.html")
-    # return "<p>Hello, World, from me R!</p>"
+
+@app.route("/about")
+def about_page():
+	return "Please subscribe  Artificial Intelligence Hub..!!!"
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/" + img.filename
+		img.save(img_path)
+
+		p = predict_label(img_path)
+
+	return render_template("index.html", prediction = p, img_path = img_path)
 
 
-# def main():
-#     print("hello")
-
-if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5000)
-    # app.run(host="127.0.0.1", port=8080, debug=True)
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(host='0.0.0.0', port=5000)
---
+++ main1.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template,request,url_for
+
+app = Flask(__name__)
+#add comment
+
+def generate_html(message):
+    version_number = '007'
+    html = """
+        <html>
+        <body>
+            <div style='text-align:center;font-size:80px;'>
+                <br> {0}
+                <p>Version Number: {1}</p>
+                <br>
+                <image height="800" width="500" src="https://raw.githubusercontent.com/richardrengel/complaint_triage/main/images/IMG-8086.jpg">
+                
+            </div>
+        </body>
+        </html>""".format(message,version_number)
+    return html
+
+def greet():
+    greeting = 'Hi, from Mushroom'
+    return greeting
+
+
+
+
+# @app.route("/")
+# def hello_world():
+#     html = generate_html(greet())
+#     return html
+@app.route("/")
+def index():
+	return render_template("index.html")
+    # return "<p>Hello, World, from me R!</p>"
+
+
+# def main():
+#     print("hello")
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(host="127.0.0.1", port=8080, debug=True)
